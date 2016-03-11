2aa206894c730e7cf5f0405565b70e8e0dbc9833
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2016-03-10 18:40:20 -0800

    rc_receiver: update code to fetch and process data directly over serial

---
+++ src/drivers/rc_receiver/dsm.h
@@ -0,0 +1,7 @@
+/*
+  declaration of dsm_decode from dsm.cpp
+ */
+bool dsm_decode(uint64_t frame_time, const uint8_t dsm_frame[16], 
+                uint16_t *values, 
+                uint16_t *num_values, 
+                uint16_t max_values);
--- src/drivers/rc_receiver/rc_receiver.cpp
+++ src/drivers/rc_receiver/rc_receiver.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (C) 2015 Mark Charlebois. All rights reserved.
+ *   Copyright (C) 2015 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,24 +30,17 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  ****************************************************************************/
-
-#include <px4_getopt.h>
 #include <px4_tasks.h>
-#include <px4_log.h>
+#include <px4_getopt.h>
+#include <px4_posix.h>
+#include <errno.h>
 #include <string.h>
 #include <drivers/drv_hrt.h>
 #include <uORB/topics/input_rc.h>
 #include <uORB/topics/parameter_update.h>
 #include <drivers/drv_rc_input.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <semaphore.h>
-#include <rc_receiver_api.h>
-#ifdef __cplusplus
-}
-#endif
+#include <dev_fs_lib_serial.h>
+#include "dsm.h"
 
 /** driver 'main' command */
 extern "C" { __EXPORT int rc_receiver_main(int argc, char *argv[]); }
@@ -66,17 +59,12 @@ char _device[MAX_LEN_DEV_PATH];
 /** flag indicating if rc_receiver module is running */
 bool _is_running = false;
 
+bool new_rc_input = false;
 /** flag indicating if task thread should exit */
 bool _task_should_exit = false;
 
 /** handle to the task main thread */
 px4_task_t _task_handle = -1;
-
-/** RC receiver serial data ready semaphore */
-sem_t _dri_semaphore;
-
-/** RC receiver type */
-enum RC_RECEIVER_TYPES _rc_receiver_type;
 
 /** RC input channel value array accomondating up to RC_INPUT_MAX_CHANNELS */
 uint16_t rc_inputs[input_rc_s::RC_INPUT_MAX_CHANNELS];
@@ -90,9 +78,13 @@ struct input_rc_s _rc_in;
 /**< parameter update subscription */
 int _params_sub;
 
+unsigned int counter = 0;
+unsigned int _num_channels = 0;
 struct {
 	param_t rc_receiver_type;
 } _params_handles;  /**< parameter handles */
+
+int serial_fd;
 
 /** Print out the usage information */
 void usage();
@@ -103,9 +95,11 @@ void start();
 /** mpu9x50 stop measurement */
 void stop();
 
+void init();
 /** task main trampoline function */
 void	task_main_trampoline(int argc, char *argv[]);
-
+void read_callback_trampoline(void *ctx, char *buf, size_t size);
+void read_callback(char *buf, size_t size);
 /** mpu9x50 measurement thread primary entry point */
 void task_main(int argc, char *argv[]);
 
@@ -114,6 +108,8 @@ void parameters_update();
 
 /** initialize all parameter handles and load the initial parameter values */
 void parameters_init();
+
+void add_dsm_input(const uint8_t *bytes, size_t nbytes);
 
 /** poll parameter update */
 void parameter_update_poll();
@@ -125,7 +121,6 @@ void parameters_update()
 
 	// accel params
 	if (param_get(_params_handles.rc_receiver_type, &v) == 0) {
-		_rc_receiver_type = (enum RC_RECEIVER_TYPES)v;
 		PX4_DEBUG("rc_receiver_parameters_update rc_receiver_type %f", v);
 	}
 }
@@ -179,16 +174,107 @@ void stop()
 	_task_handle = -1;
 }
 
+// state of add_dsm_input
+struct {
+    uint8_t frame[16];
+    uint8_t partial_frame_count;
+    uint32_t last_input_ms;
+} dsm;
+/*
+  add some bytes of input in DSM serial stream format, coping with partial packets
+ */
+void add_dsm_input(const uint8_t *bytes, size_t nbytes)
+{
+    if (nbytes == 0) {
+        return;
+    }
+    const uint8_t dsm_frame_size = sizeof(dsm.frame);
+
+    uint32_t now = hrt_absolute_time()/1000;    
+    if (now - dsm.last_input_ms > 5) {
+        // resync based on time
+        dsm.partial_frame_count = 0;
+    }
+    dsm.last_input_ms = now;
+    
+    while (nbytes > 0) {
+        size_t n = nbytes;
+        if (dsm.partial_frame_count + n > dsm_frame_size) {
+            n = dsm_frame_size - dsm.partial_frame_count;
+        }
+        if (n > 0) {
+            memcpy(&dsm.frame[dsm.partial_frame_count], bytes, n);
+            dsm.partial_frame_count += n;
+            nbytes -= n;
+            bytes += n;
+        }
+
+	if (dsm.partial_frame_count == dsm_frame_size) {
+            dsm.partial_frame_count = 0;
+            uint16_t values[16] {};
+            uint16_t num_values=0;
+            if (dsm_decode(hrt_absolute_time(), dsm.frame, values, &num_values, 16) &&
+                num_values >= 5) {
+                for (uint8_t i=0; i<num_values; i++) {
+                    if (values[i] != 0) {
+                        rc_inputs[i] = values[i];
+                    }
+                }
+                /*
+                  the apparent number of channels can change on DSM,
+                  as they are spread across multiple frames. We just
+                  use the max num_values we get
+                 */
+                if (num_values > _num_channels) {
+                    _num_channels = num_values;
+                }
+                new_rc_input = true;
+
+                //PX4_WARN("Decoded DSM %u channels %u %u %u %u %u %u %u %u\n",
+                //           (unsigned)num_values,
+                //            values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+            }
+        }
+    }
+}
+
+void init()
+{
+    serial_fd = ::open(_device, O_RDONLY|O_NONBLOCK);
+    if (serial_fd == -1) {
+        PX4_WARN("Unable to open RC input %s", _device);
+    }
+
+    struct dspal_serial_ioctl_data_rate rate;
+    rate.bit_rate = DSPAL_SIO_BITRATE_115200;
+    int ret = ioctl(serial_fd, SERIAL_IOCTL_SET_DATA_RATE, (void *)&rate);
+
+    struct dspal_serial_ioctl_receive_data_callback callback;
+    callback.rx_data_callback_func_ptr = read_callback_trampoline;
+    ret = ioctl(serial_fd, SERIAL_IOCTL_SET_RECEIVE_DATA_CALLBACK, (void *)&callback);
+}
+
+void read_callback_trampoline(void *ctx, char *buf, size_t size)
+{
+    read_callback(buf, size);
+}
+
+/*
+  callback for incoming data
+ */
+void read_callback(char *buf, size_t size)
+{
+    add_dsm_input((const uint8_t *)buf, size);
+}
+
 void task_main_trampoline(int argc, char *argv[])
 {
-	PX4_WARN("task_main_trampoline");
 	task_main(argc, argv);
 }
 
 void task_main(int argc, char *argv[])
 {
 	PX4_WARN("enter rc_receiver task_main");
-	uint32_t fd;
 
 	// clear the rc_input report for topic advertise
 	memset(&_rc_in, 0, sizeof(struct input_rc_s));
@@ -203,21 +289,11 @@ void task_main(int argc, char *argv[])
 	// subscribe to parameter_update topic
 	_params_sub = orb_subscribe(ORB_ID(parameter_update));
 
-	// Open the RC receiver device on the specified serial port
-	fd = rc_receiver_open(_rc_receiver_type, _device);
-
-	if (fd <= 0) {
-		PX4_WARN("failed to open rc receiver type %d dev %s. Quit!",
-			 _rc_receiver_type, _device);
-		return ;
-	}
-
+	init();
 	// Continuously receive RC packet from serial device, until task is signaled
 	// to exit
-	uint32_t num_channels;
+	uint32_t num_channels = 0;
 	uint64_t ts = hrt_absolute_time();
-	int ret;
-	int counter = 0;
 
 	_rc_in.timestamp_last_signal = ts;
 
@@ -225,15 +301,13 @@ void task_main(int argc, char *argv[])
 		// poll parameter update
 		parameter_update_poll();
 
-		// read RC packet from serial device in blocking mode.
-		num_channels = input_rc_s::RC_INPUT_MAX_CHANNELS;
-
-		ret = rc_receiver_get_packet(fd, rc_inputs, &num_channels);
-		ts = hrt_absolute_time();
-
+		//PX4_WARN("RC DATA: %d %d %d %d", rc_inputs[0], rc_inputs[1], rc_inputs[2], rc_inputs[3]);
+		
 		_rc_in.input_source = input_rc_s::RC_INPUT_SOURCE_QURT;
 
-		if (ret < 0) {
+		ts = hrt_absolute_time();
+
+		if (!new_rc_input) {
 			// enum RC_RECEIVER_ERRORS error_code = rc_receiver_get_last_error(fd);
 			// PX4_WARN("RC packet receiving timed out. error code %d", error_code);
 
@@ -251,9 +325,8 @@ void task_main(int argc, char *argv[])
 				continue;
 			}
 		}
-
-		// populate the input_rc_s structure
-		if (ret == 0) {
+		if (new_rc_input) {
+			num_channels = _num_channels;
 			_rc_in.timestamp_publication = ts;
 			_rc_in.timestamp_last_signal = _rc_in.timestamp_publication;
 			_rc_in.channel_count = num_channels;
@@ -264,21 +337,19 @@ void task_main(int argc, char *argv[])
 			_rc_in.rc_lost = false;
 			_rc_in.rc_lost_frame_count = 0;
 			_rc_in.rc_total_frame_count = 1;
+			new_rc_input = false;
 		}
-
 		for (uint32_t i = 0; i < num_channels; i++) {
 			// Scale the Spektrum DSM value to ppm encoding. This is for the
 			// consistency with PX4 which internally translates DSM to PPM.
 			// See modules/px4iofirmware/dsm.c for details
 			// NOTE: rc_receiver spektrum driver outputs the data in 10bit DSM
 			// format, so we need to double the channel value before the scaling
-			_rc_in.values[i] = ((((int)(rc_inputs[i]*2) - 1024) * 1000) / 1700) + 1500;
+			_rc_in.values[i] = rc_inputs[i];
 		}
-
 		orb_publish(ORB_ID(input_rc), _input_rc_pub, &_rc_in);
 	}
-
-	rc_receiver_close(fd);
+	::close(serial_fd);
 }
 
 void usage()
